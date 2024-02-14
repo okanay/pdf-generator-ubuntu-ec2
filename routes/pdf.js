3c662e6b271aad9c31490e6a60f71c759b11c389
okanay <okanay@hotmail.com>
2024-02-14 10:06:33 +0300

    puppeteer

--- routes/pdf.js
+++ routes/pdf.js
@@ -1,8 +1,24 @@
-const searchUrl = process.env.SEARCH_URL || "https://nextdok.dev/";
 const router = require("express").Router();
+const puppeteer = require('puppeteer');
+
+const searchUrl = process.env.SEARCH_URL || "https://nextdok.dev/";
 
 router.get("/pdf", async (req, res) => {
-    res.json({ searchUrl });
+
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+        });
+
+        const page = await browser.newPage();
+        await page.goto(searchUrl);
+        const title = await page.title();
+        await browser.close();
+        return res.json({title: title})
+    }
+    catch (error) {
+        return res.json({error: error.message})
+    }
 });
 
 module.exports = router;
