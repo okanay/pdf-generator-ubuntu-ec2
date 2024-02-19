d414814de487ca46b65d745ee862ced62589f254
okanay <okanay@hotmail.com>
2024-02-19 16:00:04 +0300

    server-path

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,9 +4,20 @@ import { UpdatePageDataInBackground } from "../db/functions/page-data.ts";
 import { isAuthorized } from "../security/check-auth.ts";
 
 const authorizeNeedPaths = ["/pdf"];
+const serverPaths = ["/", "/pdf", "test"];
 
 const middleware = async (c: Context, next: Next) => {
   const path = c.req.path;
+
+  if (!serverPaths.includes(path)) {
+    return c.json(
+      {
+        path,
+        message: "Not Found!",
+      },
+      404,
+    );
+  }
 
   if (authorizeNeedPaths.includes(path)) {
     if (!isAuthorized(c)) {
